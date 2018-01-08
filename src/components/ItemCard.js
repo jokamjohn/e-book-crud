a59782bab7fe90fe58c31b79999b594991a801f7
johnkagga <johnkagga@gmail.com>
2018-01-08 20:52:59 +0300

    Added is editing boolean

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const ItemCard = props => (
-    <div className="col-md-6 col-lg-3">
-      <div className="card mb-3">
-        <img className="card-img-top" src={props.image}/>
-        <div className="card-body">
-          <h4 className="card-title text-center">{props.item.name}</h4>
-          <div className="row justify-content-center mb-4">
-            <p className="card-text">
-              <span className="badge badge-secondary py-2 mr-5">Price</span>
-              <span>${props.item.price}</span>
-            </p>
-          </div>
-          <div className="row justify-content-center">
-            <div>
-              <button type="button" className="btn btn-primary mr-2">Edit</button>
-              <button type="button" className="btn btn-primary">Delete</button>
+export class ItemCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    }
+  }
+
+  toggleEdit = () => this.setState({isEditing: !this.state.isEditing});
+
+  render() {
+    const {isEditing} = this.state;
+    return (
+        <div className="col-md-6 col-lg-3">
+          <div className="card mb-3">
+            <img className="card-img-top" src={this.props.image}/>
+            <div className="card-body">
+              <h4 className="card-title text-center">{this.props.item.name}</h4>
+              <div className="row justify-content-center mb-4">
+                <p className="card-text">
+                  <span className="badge badge-secondary py-2 mr-5">Price</span>
+                  <span>${this.props.item.price}</span>
+                </p>
+              </div>
+              <div className="row justify-content-center">
+                {isEditing
+                    ?
+                    <div>
+                      <button type="button" className="btn btn-primary mr-2">Update</button>
+                      <button type="button" className="btn btn-primary" onClick={this.toggleEdit}>Cancel</button>
+                    </div>
+                    :
+                    <div>
+                      <button type="button" className="btn btn-primary mr-2" onClick={this.toggleEdit}>Edit</button>
+                      <button type="button" className="btn btn-primary">Delete</button>
+                    </div>
+                }
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-);
+    )
+  }
+}
 
 ItemCard.propTypes = {
   image: PropTypes.string.isRequired,
@@ -31,3 +53,20 @@ ItemCard.propTypes = {
     price: PropTypes.number.isRequired
   })
 };
+
+//Convert ItemCard component into a class component
+//Prefix all code instances that start props to this.props
+//the functionality should still be the same.
+
+//Should be able to edit the name or price from the card or cancel the edit.
+//Add a constructor and add state with a boolean isEditing set to false as the
+//default value.
+
+//Create a function called toggleEdit.  this will toggle the isEditing boolean in state
+//to either false/true when the Edit/Cancel buttons are clicked.
+
+//Use destructing to get the isEditing boolean value from state in teh render method.
+//then use it to show either the update/cancel buttons or the edit/delete buttons.
+//add an onclick event handler to both the cancel and edit buttons.
+//When the edit button is clicked the text of the buttons changes to update/cancel.
+//When cancel is clicked the text of the buttons changes back to edit/delete.
