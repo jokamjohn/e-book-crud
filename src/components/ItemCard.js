6ebad1a5980b0568ed06af19165d84298b04d0ba
johnkagga <johnkagga@gmail.com>
2018-01-08 21:03:35 +0300

    Toggle between input fields and name/price values

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -5,26 +5,41 @@ export class ItemCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isEditing: false
+      isEditing: false,
+      name: props.item.name,
+      price: props.item.price
     }
   }
 
   toggleEdit = () => this.setState({isEditing: !this.state.isEditing});
 
   render() {
-    const {isEditing} = this.state;
+    const {isEditing, name, price} = this.state;
     return (
         <div className="col-md-6 col-lg-3">
           <div className="card mb-3">
             <img className="card-img-top" src={this.props.image}/>
             <div className="card-body">
-              <h4 className="card-title text-center">{this.props.item.name}</h4>
-              <div className="row justify-content-center mb-4">
-                <p className="card-text">
-                  <span className="badge badge-secondary py-2 mr-5">Price</span>
-                  <span>${this.props.item.price}</span>
-                </p>
-              </div>
+              {isEditing
+                  ?
+                  <div className="mb-4">
+                    <input type="text" name="name" className="form-control mb-2 mr-sm-2" placeholder="Item"
+                           value={name} required/>
+                    <input type="number" name="price" className="form-control" placeholder="Price"
+                           value={price} required/>
+                  </div>
+                  :
+                  <div>
+                    <h4 className="card-title text-center">{name}</h4>
+                    <div className="row justify-content-center mb-4">
+                      <p className="card-text">
+                        <span className="badge badge-secondary py-2 mr-5">Price</span>
+                        <span>${price}</span>
+                      </p>
+                    </div>
+                  </div>
+              }
+
               <div className="row justify-content-center">
                 {isEditing
                     ?
@@ -70,3 +85,11 @@ ItemCard.propTypes = {
 //add an onclick event handler to both the cancel and edit buttons.
 //When the edit button is clicked the text of the buttons changes to update/cancel.
 //When cancel is clicked the text of the buttons changes back to edit/delete.
+
+//Add name and price to state and set their defaults to the name and price from the props.
+
+//Using the isEditing boolean from state we show the name and price input fields(true) or
+//the name h4  and price(false). as shown in the code.
+//At this point we cannot change the name/price nothing can be deleted or added in the input fields.
+
+
